4ee10aac394ed6417ccc9813e72d3d756190970e
Rafael <rafaelviana001@yahoo.com>
2021-09-27 20:29:26 -0300

    projeto inicial

---
+++ app/src/main/java/br/com/jogoaprender/MainActivity.java
@@ -0,0 +1,40 @@
+package br.com.jogoaprender;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
+
+import br.com.jogoaprender.Fragments.BichosFragment;
+import br.com.jogoaprender.Fragments.NumerosFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SmartTabLayout smartTabLayout;
+    private ViewPager viewPager;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        smartTabLayout = findViewById(R.id.smartTabLayout);
+        viewPager = findViewById(R.id.viewPager);
+
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(getSupportFragmentManager(),
+                FragmentPagerItems.with(this)
+                .add("Bichos", BichosFragment.class)
+                .add("Números", NumerosFragment.class)
+                .create()
+        );
+
+        viewPager.setAdapter(adapter);
+        smartTabLayout.setViewPager(viewPager);
+    }
+}
